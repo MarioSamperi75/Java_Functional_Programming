8188b8e9bd4704441c4bbebc52cc51f85f3b470b
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-01-04 12:46:47 +0100

    FP01: Filter - functional approach

--- src/FP01Functional.java
+++ src/FP01Functional.java
@@ -1,5 +1,4 @@
-// example of functional programming
-// We will write a simple program displaying all the numbers in a List
+// We want to display just the even the numbers in a List
 // first in a structured(traditional) way 
 // and then with functional programming
 
@@ -8,18 +7,27 @@ import java.util.List;
 public class FP01Functional {
 
 	public static void main(String[] args) {
-		printAllNumbersInListFunctional((List.of(23,12,5,14,11,24,18,9,5)));
-		//you can use also Arrays.asList
+		List<Integer> numbers = List.of(23,12,5,14,11,24,18,9,5);
+		//printAllNumbersInListFunctional(numbers);
+		printEvenNumbersInListFunctional(numbers);
+	}
+	
+	private static boolean isEven(int number) {
+		return number%2 == 0;
 	}
 
-	// to semplify the code	
-	// we have deleted our print method 
-	// and used the System.out.println() method directly in the for each
+
 	private static void printAllNumbersInListFunctional(List<Integer> numbers) {	
-		//we convert the array to a stream!
 		numbers.stream().
 		forEach(System.out::println);
-		//Method reference (nameOfTheClass::nameOfTheMethod)
+	}
+	
+	private static void printEvenNumbersInListFunctional(List<Integer> numbers) {	
+		numbers.stream()
+		.filter(FP01Functional::isEven)	// Filter - only allow even numbers
+		.forEach(System.out::println);
 	}
 
 }
+
+// filter do a check: only if the number is even, it's included in the forEach   
