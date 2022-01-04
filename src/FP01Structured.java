8a4211e20f9001a03c4ca5965cb46374588881c9
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-01-04 12:33:21 +0100

    FP01: Filter - traditional approach

--- src/FP01Structured.java
+++ src/FP01Structured.java
@@ -1,5 +1,4 @@
-// example of functional programming
-// We will write a simple program displaying all the numbers in a List
+// We want to display just the even the numbers in a List
 // first in a structured(traditional) way 
 // and then with functional programming
 
@@ -8,13 +7,17 @@ import java.util.List;
 public class FP01Structured {
 
 	public static void main(String[] args) {
-		printAllNumbersInListStructured((List.of(23,12,5,14,11,24,18,9,5)));
-		//you can use also Arrays.asList
+		List<Integer> numbers = List.of(23,12,5,14,11,24,18,9,5);
+		//printAllNumbersInListStructured(numbers);
+		printEvenNumbersInListStructured(numbers);
+	
 	}
 
-	private static void printAllNumbersInListStructured(List<Integer> numbers) {
+	private static void printEvenNumbersInListStructured(List<Integer> numbers) {
 		for(int number:numbers) {
-			System.out.println(number);
+			if (number%2==0) {
+				System.out.println(number);
+			}
 		}		
 	}
 
