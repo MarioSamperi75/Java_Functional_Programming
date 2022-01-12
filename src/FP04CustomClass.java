333d43f373a0637febbd8ce3c4c57c806949fc1a
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-01-12 18:10:33 +0100

    FP04 Created a Custom Class to play around

---
+++ src/FP04CustomClass.java
@@ -0,0 +1,74 @@
+import java.util.List;
+
+class Course {
+	
+	private String name;
+	private String category;
+	private int reviewScore;
+	private int noOfStudents;
+	public String getName() {
+		return name;
+	}
+	
+	
+	
+	public Course(String name, String category, int reviewScore, int noOfStudents) {
+		super();
+		this.name = name;
+		this.category = category;
+		this.reviewScore = reviewScore;
+		this.noOfStudents = noOfStudents;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public int getReviewScore() {
+		return reviewScore;
+	}
+	public void setReviewScore(int reviewScore) {
+		this.reviewScore = reviewScore;
+	}
+	public int getNoOfStudents() {
+		return noOfStudents;
+	}
+	public void setNoOfStudents(int noOfStudents) {
+		this.noOfStudents = noOfStudents;
+	}
+	@Override
+	public String toString() {
+		return "Course [name=" + name + ", category=" + category + ", reviewScore=" + reviewScore + ", noOfStudents="
+				+ noOfStudents + "]";
+	}
+	
+	
+	
+}
+
+public class FP04CustomClass {
+
+	public static void main(String[] args) {
+		
+		List<Course> courses = List.of(
+				new Course("Spring", "Framework", 98, 20000),
+				new Course("Spring Boot", "Framework", 95, 18000),
+				new Course("API", "Microservices", 97, 22000),
+				new Course("Microservices", "Microservices", 96, 25000),
+				new Course("Fullstack", "Fullstack", 91, 14000),
+				new Course("AWS", "Cloud", 92, 21000),
+				new Course("Azure", "Cloud", 99, 21000),
+				new Course("Docker", "Cloud", 92, 20000),
+				new Course("Kubernetes", "Cloud", 91, 20000)
+				);
+
+	}
+
+}
