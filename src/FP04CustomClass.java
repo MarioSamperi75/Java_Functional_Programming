4f8b2ed9bc71214a2686f71b5db730946dde4116
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-01-12 20:55:26 +0100

    FP04 Custom Class - allMatch - noneMatch - anyMatch

--- src/FP04CustomClass.java
+++ src/FP04CustomClass.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.function.Predicate;
 
 class Course {
 	
@@ -6,6 +7,8 @@ class Course {
 	private String category;
 	private int reviewScore;
 	private int noOfStudents;
+	
+	
 	public String getName() {
 		return name;
 	}
@@ -68,6 +71,37 @@ public class FP04CustomClass {
 				new Course("Docker", "Cloud", 92, 20000),
 				new Course("Kubernetes", "Cloud", 91, 20000)
 				);
+		
+		//allMatch, noneMAtch, anyMatch
+		
+
+		
+		Predicate<? super Course> reviewScoreBiggerThan90 = course -> course.getReviewScore() > 90;
+		
+		
+		Predicate<? super Course> reviewScoreLessThan90 = course -> course.getReviewScore() < 90;
+		
+		Predicate<? super Course> reviewScoreBiggerThan95 = course -> course.getReviewScore() > 95;
+		
+		System.out.println(
+				courses.stream().allMatch(course -> course.getReviewScore() > 90));
+		
+		System.out.println(
+				courses.stream().allMatch(reviewScoreBiggerThan95));
+		
+		System.out.println(
+				courses.stream().noneMatch(reviewScoreBiggerThan95));
+		
+		System.out.println(
+				courses.stream().noneMatch(reviewScoreLessThan90));		
+		
+		System.out.println(
+				courses.stream().anyMatch(reviewScoreLessThan90));	
+		
+		System.out.println(
+				courses.stream().anyMatch(reviewScoreBiggerThan95));	
+	
+		
 
 	}
 
